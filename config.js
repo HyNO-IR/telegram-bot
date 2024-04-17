20b55eef87a137505f992e75fbdabcf69bffa561
PAR3A <111696936+HyNO-IR@users.noreply.github.com>
2024-04-17 15:22:36 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { Telegraf } from 'telegraf';
 
-export const bot = new Telegraf("6973564712:AAEQeqKCGmmmvxtlYYJpMN8KzOqPqJopw_w", {
+export const bot = new Telegraf("7195161757:AAEQEG74tSEJHVeMbqkErnSalojBjdVkjo4", {
     polling: {
       interval: 300,
       autoStart: true
